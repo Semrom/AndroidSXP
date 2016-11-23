99a10c84903ca23583c7cbd352c668fbf95ec38b
jeproQxT <jeprodev@gmail.com>
2016-11-23 06:36:28 +0100

    unlock dex limit

--- AndroidSXP/app/src/androidTest/java/com/sxp/androidsxp/ExampleInstrumentedTest.java
+++ AndroidSXP/app/src/androidTest/java/com/sxp/androidsxp/ExampleInstrumentedTest.java
@@ -1,7 +1,7 @@
 package com.sxp.androidsxp;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+/*import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
@@ -13,14 +13,14 @@ import static org.junit.Assert.*;
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
+ * /
+@RunWith(AndroidJUnit4.class)*/
 public class ExampleInstrumentedTest {
-    @Test
+    //@Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
+        /* / Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sxp.androidsxp", appContext.getPackageName());
+        assertEquals("com.sxp.androidsxp", appContext.getPackageName()); */
     }
 }
